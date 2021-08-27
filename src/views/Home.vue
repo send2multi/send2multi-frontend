f70210a48a84b3d1a80b6f07d043ce7d43a05767
wangkui0508 <wangkui0508@hotmail.com>
2021-08-27 11:24:50 +0800

    Change icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Send Coins to Multiple Addresses</h1>
   <div class="normal">
-    <p>This small utility helps you send coins to multiple addresses.</p>
+    <p>This small utility helps you send coins to multiple addresses. This is a <a href="https://www.puredapp.org">Pure DApp</a>, which means its contract code and front-end code are both opensource and anyone can deploy them at anywhere.</p>
     <p>You can send the <a @click="same" href="">same amount</a> of coins to each of these addresses, or send <a @click="diff" href="">different amount</a> to different address.</p>
   </div>
 </template>
